a21697b8f3b8d5a83dca3f7c476d25b92edbfc34
Mi ordenador <miordenador@MacBook-Air-de-Mi.local>
2022-07-24 14:29:33 -0500

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xD4eE9103F8da57C3b8FEf63E379E002D2Ae71CBe";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
